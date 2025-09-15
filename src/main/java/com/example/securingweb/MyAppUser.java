55960d710edff1c46cb6f8036edc67b69b083df0
evanc7007 <evanjasonchen@gmail.com>
2025-09-14 22:49:36 -0400

    starting process of adding registration, connection to database successful

---
+++ src/main/java/com/example/securingweb/MyAppUser.java
@@ -0,0 +1,41 @@
+package com.example.securingweb;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class MyAppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getId(){
+        return id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword(){
+        return password;
+    }
+}
