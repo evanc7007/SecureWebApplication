b78014bd115c43fc738d3a59ded0a8d17fb3410c
evanc7007 <evanjasonchen@gmail.com>
2025-11-28 18:21:23 -0500

    removed useless imports, need to make it return error for username already in use

--- src/main/java/com/example/securingweb/MyAppUser.java
+++ src/main/java/com/example/securingweb/MyAppUser.java
@@ -1,5 +1,6 @@
 package com.example.securingweb;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +12,7 @@ public class MyAppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique = true, nullable = false)
     private String username;
     //private String email;
     private String password;
